03b72b598ce7bace5e737354e0798924c0572f2a
Anuradha Adhikari <adhikarianuradha336@gmail.com>
2024-10-12 08:38:26 +0530

    changes in mobile no not required
    member registration and reviewers registration

--- src/components/ReviewersRegistration.jsx
+++ src/components/ReviewersRegistration.jsx
@@ -430,7 +430,8 @@ function ReviewersRegistration() {
                     className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
                   >
                     <span className="text-xs font-medium text-gray-700">
-                      Mobile<span style={{ color: "red" }}>*</span>
+                      Mobile
+                       
                     </span>
                     <input
                       type="text"
@@ -439,7 +440,7 @@ function ReviewersRegistration() {
                       className="mt-1 w-full border-none p- 0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
                       value={mobile}
                       onChange={(e) => setMobile(e.target.value)}
-                      required
+                      // required
                     />
                   </label>
                 </div>

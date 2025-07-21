084f4ed462a436251455ebecf7a9596859115a9d
Nilanjana Chaudhuri <145705332+Nilanjana0509@users.noreply.github.com>
2025-07-21 23:57:28 +0530

    Reverted changes made to this case for sentence case
    This change felt unnecessary so I reverted it back to original state

--- src/components/TrackRegistration.jsx
+++ src/components/TrackRegistration.jsx
@@ -269,7 +269,7 @@ function TrackRegistration() {
                 {tracks.map((track) => (
                   <tr key={track._id}>
                     <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    {track.track_name}
+                    {toSentenceCase(track.track_name)}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                       <button
